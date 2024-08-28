785617691636178256c7972fc9d22656dabddafb
matheusft20 <nubzeiraplay@gmail.com>
2024-08-28 12:01:42 -0300

    primeiro deploy

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Seja bem vindo ao meu primeiro Deploy.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
